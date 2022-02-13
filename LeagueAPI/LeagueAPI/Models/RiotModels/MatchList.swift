0e32cf765a50793806a7ef18ebe433ffb1886ef0
Bilel <bilel@powder.gg>
2022-02-13 19:58:39 +0100

    Fix

--- LeagueAPI/LeagueAPI/Models/RiotModels/MatchList.swift
+++ LeagueAPI/LeagueAPI/Models/RiotModels/MatchList.swift
@@ -12,16 +12,8 @@ public class MatchList: Decodable {
     
     public var matches: [String]
     
-    enum CodingKeys: String, CodingKey {
-        case matches = "matches"
-    }
-    
-    public init(totalGames: Int, startIndex: Int, endIndex: Int, matches: [String]) {
-        self.matches = matches
-    }
-    
     public required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.matches = try container.decode([String].self, forKey: .matches)
+        var container = try decoder.unkeyedContainer()
+        self.matches = try container.decode([String].self)
     }
 }
